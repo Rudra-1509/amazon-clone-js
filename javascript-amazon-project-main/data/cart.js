4872aa06943cd99e17eba5da33eacae814ec5090
Rudra <rudranilmn1509@gmail.com>
2024-10-21 22:16:08 +0530

    delete interaction

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,31 +1,40 @@
- export const cart=
-[
-    {
-        productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
-        cartQuantity:1
-
-    },
-    {
-        productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-        cartQuantity:1
-    }
+export let cart = [
+  {
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    cartQuantity: 1,
+  },
+  {
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    cartQuantity: 2,
+  },
 ];
 
 export function addToCart(productId) {
-    let matchingItem;
-  
-    cart.forEach((item) => {
-      if (productId === item.productId) {
-        matchingItem = item;
-      }
-    });
-  
-    if (matchingItem) {
-      matchingItem.quantity += 1;
-    } else {
-      cart.push({
-        productId: productId,
-        quantity: 1,
-      });
+  let matchingItem;
+
+  cart.forEach((item) => {
+    if (productId === item.productId) {
+      matchingItem = item;
     }
+  });
+
+  if (matchingItem) {
+    matchingItem.quantity += 1;
+  } else {
+    cart.push({
+      productId: productId,
+      quantity: 1,
+    });
   }
+}
+
+export function deleteFromCart(productId) {
+  let newCart = [];
+
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem);
+    }
+  });
+  cart = newCart;
+}
--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -1,21 +1,18 @@
-import {cart} from '../data/cart.js';
-import { products } from '../data/products.js';
-import {formatCurrency} from './utils/money.js';
+import { cart, deleteFromCart } from "../data/cart.js";
+import { products } from "../data/products.js";
+import { formatCurrency } from "./utils/money.js";
 
-let cartSumamryHtml='';
-cart.forEach((cartItem)=>{
+let cartSumamryHtml = "";
+cart.forEach((cartItem) => {
+  const productId = cartItem.productId;
+  let matchingProduct;
+  products.forEach((product) => {
+    if (product.id === productId) {
+      matchingProduct = product;
+    }
+  });
 
-    const productId=cartItem.productId;
-    let matchingProduct;
-    products.forEach((product)=>{
-        if(product.id===productId)
-        {
-            matchingProduct=product;
-        }
-    });
-
-    cartSumamryHtml+=
-    `<div class="cart-item-container">
+  cartSumamryHtml += `<div class="cart-item-container js-container-${cartItem.productId}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -33,12 +30,16 @@ cart.forEach((cartItem)=>{
                 </div>
                 <div class="product-quantity">
                   <span>
-                    Quantity: <span class="quantity-label">${cartItem.cartQuantity}</span>
+                    Quantity: <span class="quantity-label">${
+                      cartItem.cartQuantity
+                    }</span>
                   </span>
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary">
+                  <span class="delete-quantity-link link-primary  js-delete-quantity" data-product-id="${
+                    matchingProduct.id
+                  }"'>
                     Delete
                   </span>
                 </div>
@@ -92,4 +93,14 @@ cart.forEach((cartItem)=>{
           </div>`;
 });
 
-document.querySelector('.js-order-summary').innerHTML=cartSumamryHtml;
+document.querySelector(".js-order-summary").innerHTML = cartSumamryHtml;
+
+document.querySelectorAll(".js-delete-quantity").forEach((link) => {
+  link.addEventListener("click", () => {
+    const productId = link.dataset.productId;
+    deleteFromCart(productId);
+    const container= document.querySelector(`.js-container-${productId}`);
+    container.remove();
+  });
+});
+
