5db48409861204b86be05f17d91c0da668a85fb1
Rudra <rudranilmn1509@gmail.com>
2024-10-27 20:06:48 +0530

    remove default test and add money test

---
+++ javascript-amazon-project-main/testJasmine/testMoney.js
@@ -0,0 +1,20 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
+describe("test suite : formatCurrency", () => {
+  it("Converts cents to dollars", () => {
+    expect(formatCurrency(2095)).toEqual("20.95");
+  });
+
+  it("Works with 0", () => {
+    expect(formatCurrency(0)).toEqual("0.00");
+  });
+  describe("Rounding", () => {
+    it("Rounds up >=5", () => {
+      expect(formatCurrency(2200.5)).toEqual("22.01");
+    });
+
+    it("Rounds up <5", () => {
+      expect(formatCurrency(2200.4)).toEqual("22.00");
+    });
+  });
+});
