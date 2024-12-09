813dbbcc1cc49b1797f8f3d5dcebd8046d84d68e
Rudra <rudranilmn1509@gmail.com>
2024-12-09 11:32:17 +0530

    Add backend pratice file

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -2,6 +2,6 @@ import { renderOrderSummary } from "./checkout/ordersummary.js";
 import { renderPaymentSummary } from "./checkout/paymentsummary.js";
 // import "../data/cart-oop.js";
 // import "../data/cart-class.js";
-
+import "../data/backend-practice.js";
 renderOrderSummary();
 renderPaymentSummary();
