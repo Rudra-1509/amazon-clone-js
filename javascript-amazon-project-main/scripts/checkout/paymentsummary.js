158a556c61be4f79b31d894a4f6e72f1574eaffb
Rudra <rudranilmn1509@gmail.com>
2024-12-11 13:03:06 +0530

    create order using backend

--- javascript-amazon-project-main/scripts/checkout/paymentsummary.js
+++ javascript-amazon-project-main/scripts/checkout/paymentsummary.js
@@ -1,36 +1,40 @@
-import {cart} from "../../data/cart.js" 
+import { cart } from "../../data/cart.js";
 import { products } from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
-import {deliveryOptions} from "../../data/deliveryOptions.js";
+import { deliveryOptions } from "../../data/deliveryOptions.js";
+import { orders,addorder } from "../../data/orders.js";
 
+export function renderPaymentSummary() {
+  const itemsTotal = calcItemsTotal();
+  const shipCharge = getShipCharge();
+  const beforeTax = itemsTotal + shipCharge;
+  const tax = beforeTax * 0.1;
+  const total = beforeTax + tax;
 
-export function renderPaymentSummary()
-{
-    const itemsTotal=calcItemsTotal();
-    const shipCharge=getShipCharge();
-    const beforeTax=itemsTotal+shipCharge;
-    const tax=beforeTax*0.1;
-    const total=beforeTax+tax;
-
-    let html=
-    `
+  let html = `
     <div class="payment-summary-title">
             Order Summary
           </div>
 
           <div class="payment-summary-row">
             <div>Items (3):</div>
-            <div class="payment-summary-money">$${formatCurrency(itemsTotal)}</div>
+            <div class="payment-summary-money">$${formatCurrency(
+              itemsTotal
+            )}</div>
           </div>
 
           <div class="payment-summary-row">
             <div>Shipping &amp; handling:</div>
-            <div class="payment-summary-money">$${formatCurrency(shipCharge)}</div>
+            <div class="payment-summary-money">$${formatCurrency(
+              shipCharge
+            )}</div>
           </div>
 
           <div class="payment-summary-row subtotal-row">
             <div>Total before tax:</div>
-            <div class="payment-summary-money">$${formatCurrency(beforeTax)}</div>
+            <div class="payment-summary-money">$${formatCurrency(
+              beforeTax
+            )}</div>
           </div>
 
           <div class="payment-summary-row">
@@ -43,35 +47,52 @@ export function renderPaymentSummary()
             <div class="payment-summary-money">$${formatCurrency(total)}</div>
           </div>
 
-          <button class="place-order-button button-primary">
+          <button class="place-order-button button-primary js-place-order">
             Place your order
           </button>
     `;
-
-    document.querySelector('.js-payment-summary').innerHTML=html;
+  document.querySelector(".js-payment-summary").innerHTML = html;
+   document.querySelector('.js-place-order').addEventListener('click',async () => {
+    try{
+      const response=await fetch("https://supersimplebackend.dev/orders", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({
+          cart: cart,
+        }),
+      });
+      const order=await response.json();
+      addorder(order);
+      
+    }catch(error){
+      console.log('Error occured.please try again later.')
+    }
+    window.location.href='orders.html';
+  });
 }
 
-
-function calcItemsTotal()
-{
-  let total=0;
-  cart.forEach((item)=>{
-    let targetProduct=products.find((product)=>item.productId === product.id);
-    let price=(item.cartQuantity)*(targetProduct.priceCents);
-    total+=price;
+function calcItemsTotal() {
+  let total = 0;
+  cart.forEach((item) => {
+    let targetProduct = products.find(
+      (product) => item.productId === product.id
+    );
+    let price = item.cartQuantity * targetProduct.priceCents;
+    total += price;
   });
   return total;
 }
 
-function getShipCharge()
-{
-    let total=0;
-    cart.forEach((item)=>{
-        let targetDelOption=deliveryOptions.
-            find((deliveryOption)=>deliveryOption.id===item.deliveryOptionsID);
-        let price =targetDelOption.priceCents;
-        total+=price;
-    })
-    return total;
+function getShipCharge() {
+  let total = 0;
+  cart.forEach((item) => {
+    let targetDelOption = deliveryOptions.find(
+      (deliveryOption) => deliveryOption.id === item.deliveryOptionsID
+    );
+    let price = targetDelOption.priceCents;
+    total += price;
+  });
+  return total;
 }
-
